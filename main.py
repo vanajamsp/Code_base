f519eded2b8f26468998bab4d37ddf18879d2990
Vanaja Ramalingam <vramalingam@resultant.com>
2021-08-12 16:33:07 -0400

    feature_798 added few lines of code.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ x = 5
 y = 10
 
 # To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
+# x = input('Enter value of x: ')
+# y = input('Enter value of y: ')
 
 # create a temporary variable and swap the values
 temp = x
@@ -15,21 +15,26 @@ y = temp
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 
-
 # Python program to display universal hello program.!
 print('Hello World')
-
 
 # Find square root of real or complex numbers
 # Importing the complex math module
 import cmath
 
-num = 1+2j
+num = 1 + 2j
 
 # To take input from the user
-#num = eval(input('Enter a number: '))
+# num = eval(input('Enter a number: '))
 
 num_sqrt = cmath.sqrt(num)
-print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num, num_sqrt.real, num_sqrt.imag))
 
-# ########
+
+def sum(a, b):
+    return a + b
+
+
+num1 = int(input("Enter the number1 to add : "))
+num2 = int(input("Enter the number2 : "))
+print(sum(num1, num2))

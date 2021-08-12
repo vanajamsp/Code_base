7457c0506f93be0f61d6f665a044dc574471a80c
Vanaja Ramalingam <vramalingam@resultant.com>
2021-08-12 15:38:53 -0400

    Added more code.

--- main.py
+++ main.py
@@ -14,3 +14,22 @@ y = temp
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+
+# Python program to display universal hello program.!
+print('Hello World')
+
+
+# Find square root of real or complex numbers
+# Importing the complex math module
+import cmath
+
+num = 1+2j
+
+# To take input from the user
+#num = eval(input('Enter a number: '))
+
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+# ########
